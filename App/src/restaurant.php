0a7ecc932c3d11ecbea32fb01b9ab928e2148690
Coralie Larpenteur <cocolarpenteur@gmail.com>
2018-12-07 04:25:26 +0100

    restaurant.php

---
+++ App/src/restaurant.php
@@ -0,0 +1,32 @@
+<?php require_once ('../Base-Part/Header-Part/header.php')?>
+<div id="explication">
+    <div id="right">
+        <div id="back-list">
+    <span id="button-back" class="btn">
+        <i class="fas fa-angle-left"></i> Retour à la liste
+    </span>
+        </div>
+        <div id="restaurant-name" class="title space-bottom">
+            <h2>Nom du restaurant</h2>
+        </div>
+        <div id="adress" class="space-bottom">
+    <span>
+        Adresse
+    </span>
+        </div>
+        <div id="cuisine-type" class="space-bottom">
+            Type de cuisine
+        </div>
+        <div id="description" class="space-bottom">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus autem consectetur consequatur dolore doloribus error hic incidunt ipsam, labore libero nam quae quasi quisquam.
+        </div>
+        <div>btn</div>
+        <div>btn</div>
+    </div>
+    <div id="left">
+        <img src="http://via.placeholder.com/350x250" alt="" style="width: 100%; height: 100%">
+    </div>
+</div>
+
+
+<?php require_once ('../Base-Part/Footer-Part/footer.php')?>
