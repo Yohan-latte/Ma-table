874422185d413f71fda99d6b588be59bb35ff7a8
Coralie Larpenteur <cocolarpenteur@gmail.com>
2018-12-07 02:20:42 +0100

    walla

---
+++ restaurant.php
@@ -0,0 +1,12 @@
+<?php require_once ('header.php')?>
+
+<div id="back-list">
+    <span id="button-back">
+        <i class="fas fa-angle-left"></i> Retour à la liste
+    </span>
+</div>
+<div id="restaurant-name" class="title">
+    <span>Nom du restaurant</span>
+</div>
+
+<?php require_once ('footer.php')?>
