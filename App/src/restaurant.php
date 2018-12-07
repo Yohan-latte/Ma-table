4e7418adb92158417416eaeee08da46dddb3a70d
Coralie Larpenteur <cocolarpenteur@gmail.com>
2018-12-07 05:40:13 +0100

    restaurant.php 2

--- App/src/restaurant.php
+++ App/src/restaurant.php
@@ -1,32 +1,40 @@
 <?php require_once ('../Base-Part/Header-Part/header.php')?>
-<div id="explication">
+<section id="explication">
     <div id="right">
-        <div id="back-list">
-    <span id="button-back" class="btn">
-        <i class="fas fa-angle-left"></i> Retour à la liste
-    </span>
+        <div id="back-list" class="link">
+            <span id="button-back">
+                <i class="fas fa-angle-left"></i> Retour à la liste
+            </span>
         </div>
         <div id="restaurant-name" class="title space-bottom">
             <h2>Nom du restaurant</h2>
         </div>
         <div id="adress" class="space-bottom">
-    <span>
-        Adresse
-    </span>
+            <span>
+                Adresse
+            </span>
         </div>
         <div id="cuisine-type" class="space-bottom">
             Type de cuisine
         </div>
         <div id="description" class="space-bottom">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus autem consectetur consequatur dolore doloribus error hic incidunt ipsam, labore libero nam quae quasi quisquam.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at autem, deserunt eos error exercitationem fugiat incidunt molestiae nobis nulla praesentium quod, sunt! Assumenda distinctio excepturi illo repellendus sapiente. Exercitationem. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda earum, error itaque iusto natus omnis provident. A ab accusantium consequuntur, corporis earum, enim facere harum labore perspiciatis repudiandae sit, voluptate.
         </div>
-        <div>btn</div>
-        <div>btn</div>
+        <div class="duble-button">
+            <div class="btn link">Résever un table</div>
+            <span>Ou</span>
+            <div class="btn link">Se faire livrer</div>
+        </div>
     </div>
     <div id="left">
-        <img src="http://via.placeholder.com/350x250" alt="" style="width: 100%; height: 100%">
+        <img src="../img/plat.jpg" alt="" style="width: 100%; height: 100%">
     </div>
-</div>
+</section>
+<section id="menu">
+    <div class="title">
+        <h2>Menu</h2>
+    </div>
+</section>
 
 
 <?php require_once ('../Base-Part/Footer-Part/footer.php')?>
